c55e9bf5a1dbc8dd26b15f780f5e1e6b72fafd74
ARIIK-ANTHONY <a.ariik@alustudent.com>
2025-02-27 18:03:50 +0200

    add changes

--- api_advanced/2-recurse.py
+++ api_advanced/2-recurse.py
@@ -6,22 +6,39 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
-    """ function that queries the Reddit API and returns a list containing
-        the titles of all hot articles for a given subreddit. If no results
-        are found for the given subreddit, the function should return None.
+    """ 
+    Function that queries the Reddit API and returns a list containing
+    the titles of all hot articles for a given subreddit. If no results
+    are found for the given subreddit, the function should return None.
+    The function is recursive and keeps calling itself until all posts 
+    are retrieved.
     """
     headers = {'User-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_0)\
                 AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100\
                 Safari/537.36'}
     params = {'limit': 100, 'after': after}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    
+    # Send GET request to Reddit API
     res = requests.get(url, headers=headers, params=params)
+    
+    # If subreddit does not exist (404), return None
     if res.status_code == 404:
         return None
-    children = res.json().get('data').get('children')
+    
+    # Parse the response to get the list of hot posts
+    children = res.json().get('data', {}).get('children', [])
+    
+    # Append the titles of the posts to hot_list
     for child in children:
-        hot_list.append(child.get('data').get('title'))
-    after = res.json().get('data').get('after')
+        hot_list.append(child.get('data', {}).get('title'))
+    
+    # Check if there is a next set of posts (pagination)
+    after = res.json().get('data', {}).get('after')
+    
+    # If there is no "after", we have reached the end of the posts
     if after is None:
         return hot_list
+    
+    # Otherwise, recurse to fetch the next set of posts
     return recurse(subreddit, hot_list, after)
